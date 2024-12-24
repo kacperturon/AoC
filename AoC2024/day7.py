e6226ce1872901a3e4834d7c3022f05b6f0e6d6d
Kacper Turon <kacperturon@KMBP.local>
2024-12-24 16:31:53 +0100

    day 7 part 2 2024

--- AoC2024/day7.py
+++ AoC2024/day7.py
@@ -1,5 +1,6 @@
 
-from helpers import runTest, get_file_lines, get_file_str
+from helpers import runTest, get_file_lines
+import itertools
 
 test = """190: 10 19
 3267: 81 40 27
@@ -17,58 +18,26 @@ else:
     input = get_file_lines('day7.txt')
     input = [line.strip() for line in input]
 
-
-operators = ['+', '*', '||']
-# input = ["292: 11 6 16 20"]
-# input = ["190: 10 19"]
-# input = ["292: 11 6 16 20"]
-
-# testval_nums = {16159: [6, 5, 8, 839, 218]}
-#  3267: [81, 40, 27]
-#  3 nums
-#  + + 
-#  + *
-#  * +
-#  * *
-# (3-1)^2
-
-def get_set_bit(val, idx):
-    # Shift the bit at index to the least significant bit and check if it's 1
-    return (val & (1 << idx)) != 0
-
-# 3749
-# 11 + 6 * 16 + 20.
-# + + +
-# * + +
-# + * +
-# + + *
-# * * +
-# + * *
-# * + *
-# * * *
-def check_equation(test, nums):
-    combinations = (len(nums)-1)**len(operators)-1
-    if len(nums) == 2:
-        combinations = 3
-    # print('comb', combinations)
+def check_equation(test, nums, operator_count):
+    if operator_count == 2:
+        operators = ['+', '*']
+    if operator_count == 3:
+        operators = ['+', '*', '|']
+    combinations = [''.join(comb) for comb in itertools.product(operators, repeat=len(nums)-1)]
     
-    for i in range(combinations):
+    for comb in combinations:
         sum = nums[0]
-        # print('start', sum, bin(i))
         for j in range(1, len(nums)):
-            # print('h', nums[j], j-1, get_set_bit(i, j-1), sum)
-            if get_set_bit(i, j-1):
+            if comb[j-1] == "+":
+                sum += nums[j]
+            elif comb[j-1] == "*":
                 sum *= nums[j]
             else:
-                sum += nums[j]
-        # print(sum)
+                sum = int(str(sum)+str(nums[j]))
         if sum == test:
-            # print('pass', sum)
             return True
     return False
 
-
-testval_nums = {}
 calibration_sum = 0
 
 for line in input:
@@ -77,21 +46,18 @@ for line in input:
     testval = int(testval)
     for num in nums.strip().split(' '):
         row.append(int(num))
-    if check_equation(testval, row) > 0:
+    if check_equation(testval, row, 2) > 0:
         calibration_sum += testval
-    # testval_nums[int(testval)] = row
-
-# calibration_sum = 0
-# for test, nums in testval_nums.items():
-#     if check_equation(test, nums) > 0:
-#         calibration_sum += test
-
 print(calibration_sum)
 
-# 
-# 6+5+8*839+218
-# 
-# 85648246569 - too low
-# 85648246694
-# 4364915411363 - correct answer
-# 4364915411363
+calibration_sum = 0
+
+for line in input:
+    row = []
+    testval, nums = line.split(':')
+    testval = int(testval)
+    for num in nums.strip().split(' '):
+        row.append(int(num))
+    if check_equation(testval, row, 3) > 0:
+        calibration_sum += testval
+print(calibration_sum)
