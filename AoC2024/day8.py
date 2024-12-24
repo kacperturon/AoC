7b1a0f8d189df4f0f9cc6c5307d867c2c3666ba2
Kacper Turon <kacperturon@KMBP.local>
2024-12-24 22:31:16 +0100

    day 8 part 2 2024

--- AoC2024/day8.py
+++ AoC2024/day8.py
@@ -21,16 +21,17 @@ else:
 
 H = len(input)
 W = len(input[0])
-grid = {(y,x):[input[y][x]] for y in range(H) for x in range(W)}
-antennas =  {}
-for y,x in grid:
-    val = grid.get((y,x))[0]
-    if val != '.':
-        if val in antennas:
-            antennas[val].append((y,x))
-        else:
-            antennas[val] = [(y,x)]
 
+def get_antennas(grid):
+    antennas =  {}
+    for y,x in grid:
+        val = grid.get((y,x))[0]
+        if val != '.':
+            if val in antennas:
+                antennas[val].append((y,x))
+            else:
+                antennas[val] = [(y,x)]
+    return antennas
 
 def print_grid(grid):
     for y in range(H):
@@ -40,25 +41,38 @@ def print_grid(grid):
         print(line)
     print()
 
-antinode_count = 0
-for y,x in grid:
-    val = grid.get((y,x))[0]
-    if val != '.' and val != '#':
-        for antenna in antennas[val]:
-            if antenna[0] != y and antenna[1] != x:
-                diff_y = y-antenna[0]
-                diff_x = x-antenna[1]
-                antinode_y = y + diff_y
-                antinode_x = x + diff_x
+def answer(part):
+    grid = {(y,x):[input[y][x]] for y in range(H) for x in range(W)}
+    antennas = get_antennas(grid)
+    antinode_count =  0
 
+    if part == 2:
+        for antenna in antennas:
+            if len(antennas[antenna]) > 1:
+                antinode_count += len(antennas[antenna])
 
-                if antinode_y >= 0 and antinode_y < H and antinode_x >= 0 and antinode_x < W and (antinode_y, antinode_x) not in antennas[val]:
-                    antinode = grid.get((antinode_y, antinode_x))
-                    if val not in antinode and '#' not in antinode:
-                        antinode_count += 1
-                        if '.' in antinode:
-                            grid[(antinode_y, antinode_x)][0] = '#'
-                        else:
-                            grid[(antinode_y, antinode_x)].append('#')
+    for y,x in grid:
+        val = grid.get((y,x))[0]
+        if val != '.' and val != '#':
+            for antenna in antennas[val]:
+                if antenna[0] != y and antenna[1] != x:
+                    diff_y = y-antenna[0]
+                    diff_x = x-antenna[1]
+                    antinode_y = y + diff_y
+                    antinode_x = x + diff_x
+                    while antinode_y >= 0 and antinode_y < H and antinode_x >= 0 and antinode_x < W:
+                        antinode = grid.get((antinode_y, antinode_x))
+                        if '#' not in antinode and part == 1 or '.' in grid.get((antinode_y, antinode_x)) and part == 2:
+                            antinode_count += 1
+                            if '.' in antinode:
+                                grid[(antinode_y, antinode_x)][0] = '#'
+                            else:
+                                grid[(antinode_y, antinode_x)].append('#')
+                        antinode_y += diff_y
+                        antinode_x += diff_x 
+                        if part == 1:
+                            break
+    return antinode_count
 
-print(antinode_count)
+print(answer(1))
+print(answer(2))
