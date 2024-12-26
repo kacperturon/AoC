17452702739151fd7103c645e87e39d44b3506e7
Kacper Turon <kacperturon@KMBP.local>
2024-12-26 14:26:45 +0100

    day 10 part 2 2024

--- AoC2024/day10.py
+++ AoC2024/day10.py
@@ -1,11 +1,11 @@
 from helpers import runTest, get_file_lines
 
-test2="""0123
+test="""0123
 1234
 8765
 9876"""
 
-test="""89010123
+test2="""89010123
 78121874
 87430965
 96549874
@@ -14,15 +14,23 @@ test="""89010123
 01329801
 10456732"""
 
+test3 = """7777707
+1143217
+1159927
+1165437
+1179947
+1187657
+1191117"""
+
 if runTest:
-    input = test.splitlines()
+    input = test3.splitlines()
 else: 
     input = get_file_lines('day10.txt')
     input = [line.strip() for line in input]
 
 H = len(input)
 W = len(input[0])
-grid = {(y, x):[int(input[y][x]), 0] for y in range(H) for x in range(W)}
+grid = {(y, x):int(input[y][x]) for y in range(H) for x in range(W)}
 neighbors = [(0,1), (0,-1), (1,0), (-1,0)]
 
 def print_trail(grid, vals):
@@ -37,38 +45,45 @@ def print_trail(grid, vals):
     print()
 
 def valid_trails(grid, y, x):
-    visited = set([])
     queue = [(y,x)]
+    visited = set()
     count = 0
-    counted_9s = set([])
     while len(queue) > 0:
-        # print_trail(grid, queue)
-        node = queue.pop(0)
-        node_val = grid[(node[0], node[1])]
-        visited.add((node[0], node[1]))
-        new_y = node[0]
-        new_x = node[1]
-        for neighbor in neighbors:
-            new_y = node[0] + neighbor[0]
-            new_x = node[1] + neighbor[1]
-            if (new_y, new_x) in grid and (new_y, new_x) not in visited:
-                new_val = grid[(new_y, new_x)]
-                if new_val[0] > node_val[0] and new_val[0] - node_val[0] == 1:
-                    if new_val[0] == 9 and (new_y, new_x) not in counted_9s:
-                        counted_9s.add((new_y, new_x))
-                        count += 1
-                    else:
-                        if (new_y, new_x) not in queue:
-                            queue.append((new_y, new_x))
+        node_y, node_x = queue.pop(0)
+        node = grid[(node_y, node_x)]
+        if (node_y, node_x) in visited:
+            continue
+        visited.add((node_y, node_x))
+        if node == 9:
+            count += 1
+        
+        for dx, dy in [(0,1), (0,-1), (1,0), (-1,0)]:
+            if (node_y+dy, node_x+dx) in grid:
+                node_next = grid[(node_y+dy),(node_x+dx)]
+                if node_next - node == 1:
+                    queue.append(((node_y+dy), (node_x+dx)))
     return count
 
+sum = 0
+for y,x in grid:
+    if grid[(y,x)] == 0:
+        sum +=  valid_trails(grid, y, x)
+print(sum)
+
+def find_trails_2(grid, y, x, trail):
+    if grid[(y,x)] == 9:
+        return [trail]
+
+    trails = []
+    for dy, dx in [(0,1), (0,-1), (1,0), (-1,0)]:
+        if ((y+dy), (x+dx)) in grid:
+            if grid[(y+dy, x+dx)] - grid[(y,x)] == 1:
+                trails += find_trails_2(grid, y+dy, x+dx, trail + [(y+dy, x+dx)])
+    return trails
 
 sum = 0
-trialhead_sums = ""
-for (y, x) in grid:
-    if grid[(y,x)][0] == 0:
-        grid[(y,x)][1] = valid_trails(grid, y,x)
-        trialhead_sums += str(grid[(y,x)][1]) + " "
-        sum += grid[(y,x)][1]
+for y,x in grid:
+    if grid[(y,x)] == 0:
+        sum += len(find_trails_2(grid, y, x, []))
 
 print(sum)
